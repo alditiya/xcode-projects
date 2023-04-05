24220e15e7ba824b2435b222be8107c15ddeebc4
Alditiya Yudha Pratama <aldit4@gmail.com>
2023-04-05 21:17:10 +0700

    Initial Commit

---
+++ xcode-project/xcode_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_projectApp.swift
+//  xcode-project
+//
+//  Created by Alditiya Yudha Pratama on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct xcode_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
